28a2eb3dbb60b3affdefa53d80952115eb52c3e9
iocdawm9 <115738128+iocdawm9@users.noreply.github.com>
2023-01-25 19:52:42 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# form-inici
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/float-mtzfmm)
---
+++ app/full-example.ts
@@ -0,0 +1,57 @@
+import {Component} from '@angular/core';
+
+/**
+ * @title Basic checkboxes
+ */
+@Component({
+  selector: 'full-example',
+  templateUrl: 'full-example.html',
+  styleUrls: ['full-example.css'],
+})
+export class CheckboxOverviewExample {
+
+  navItems = [
+    {
+      name: 'Home',
+      icon: 'home',
+      href: '/'
+    }
+  ];
+
+  searches = [
+    {
+      name: 'Dog Search',
+      icon: 'search',
+      href: '/runner-search'
+    },
+    {
+      name: 'Meeting Search',
+      icon: 'search',
+      href: '/meeting-search'
+    },
+    {
+      name: 'Owner Search',
+      icon: 'search',
+      href: '/owner-search'
+    },
+    {
+      name: 'Trainer Search',
+      icon: 'search',
+      href: '/trainer-search'
+    }
+  ];
+
+  creating = [
+    {
+      name: 'New Meeting',
+      icon: 'create',
+      href: '/new-meeting'
+    }
+  ]
+
+}
+
+
+/**  Copyright 2018 Google Inc. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at http://angular.io/license */
